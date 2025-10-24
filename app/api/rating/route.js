668ae1b8a25173eaa273b8b75328c8d91696b769
mhoa236 <mhoapt2306@gmail.com>
2025-10-25 01:11:35 +0700

    debug

--- app/api/rating/route.js
+++ app/api/rating/route.js
@@ -1,6 +1,5 @@
 import prisma from "@/lib/prisma";
 import { getAuth } from "@clerk/nextjs/server";
-import { jsonErrorSchema } from "inngest/types";
 import { NextResponse } from "next/server";
 
 
--- app/api/store/orders/route.js
+++ app/api/store/orders/route.js
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import authSeller from "@/middlewares/authSeller";
-import { auth, getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
